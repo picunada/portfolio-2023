df795ea66663e4bf19ba7491a917f52edb87babc
Daniil Bezuglov <xlaystgoku@gmail.com>
2023-09-06 15:58:39 +0700

    fix: fixed arrows on project pages

--- components/Hero.vue
+++ components/Hero.vue
@@ -42,7 +42,7 @@ onMounted(() => {
         </div>
       </div>
 
-      <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 24 24">
+      <svg class="arrow mouse-sm" xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 24 24">
         <path class="pulse" fill="currentColor" d="M12 14.975q-.2 0-.375-.062T11.3 14.7l-4.6-4.6q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l3.9 3.9l3.9-3.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7l-4.6 4.6q-.15.15-.325.213t-.375.062Z" />
       </svg>
 
@@ -108,12 +108,12 @@ onMounted(() => {
 }
 
 .arrow {
+  opacity: 0;
   position: absolute;
   bottom: 0;
   right: 0;
-  translate: -48px -50%;
+  translate: calc(100vw/2560 * -48) -50%;
   z-index: 3;
-  mix-blend-mode: overlay;
 }
 
 .pulse {
