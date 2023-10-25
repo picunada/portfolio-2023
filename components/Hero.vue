bcdb8b9330503fb4c063b36b708c4e15ad4ac2ac
picunada <xlaystgoku@gmail.com>
2023-10-25 12:06:10 +0700

    chore: changed text in index page and added experience

--- components/Hero.vue
+++ components/Hero.vue
@@ -43,7 +43,8 @@ onMounted(() => {
       </div>
 
       <svg class="arrow mouse-sm" xmlns="http://www.w3.org/2000/svg" width="96" height="96" viewBox="0 0 24 24">
-        <path class="pulse" fill="currentColor" d="M12 14.975q-.2 0-.375-.062T11.3 14.7l-4.6-4.6q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l3.9 3.9l3.9-3.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7l-4.6 4.6q-.15.15-.325.213t-.375.062Z" />
+        <path class="pulse" fill="currentColor"
+          d="M12 14.975q-.2 0-.375-.062T11.3 14.7l-4.6-4.6q-.275-.275-.275-.7t.275-.7q.275-.275.7-.275t.7.275l3.9 3.9l3.9-3.9q.275-.275.7-.275t.7.275q.275.275.275.7t-.275.7l-4.6 4.6q-.15.15-.325.213t-.375.062Z" />
       </svg>
 
       <div class="blur" />
@@ -57,7 +58,6 @@ onMounted(() => {
   /* position: absolute; */
   width: 100vw;
   height: 100vh;
-  //overflow: hidden;
   top: 0;
   left: 0;
   z-index: -10;
